91e44b6a762f212060f93caac035356d0e5b94b4
JessiP23 <jessi316866@gmail.com>
2024-10-01 10:10:52 -0400

    tomr

--- app/waitress/page.js
+++ app/waitress/page.js
@@ -82,7 +82,7 @@ export default function Waitress() {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Take Order</h1>
+      <h1 className="text-2xl font-bold mb-4">Tomar orden</h1>
 
       <div className="grid grid-cols-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {products.map((product) => {
