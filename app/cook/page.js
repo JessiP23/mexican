012f39e9915cce8d7c8fe1c5ee97b325cea1c4e1
JessiP23 <jessi316866@gmail.com>
2024-10-01 23:36:09 -0400

    eng

--- app/cook/page.js
+++ app/cook/page.js
@@ -7,6 +7,7 @@ import Sidebar from "./_components/Sidebar"
 
 export default function Cook() {
   const [orders, setOrders] = useState([])
+  
   const [completedOrders, setCompletedOrders] = useState([]);
 
 
